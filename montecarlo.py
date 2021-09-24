ccb9272726c33817142b10e21ebeba1fbe8aab1c
jm-wltr <90411864+jm-wltr@users.noreply.github.com>
2021-09-24 13:47:23 +0200

    Made faster calculation algorithm
    I changed the algorithm so that it does not start calculating from scratch each time, but instead uses the previous calculations and build on them. It is, indeed, much faster, so much so that it can reach 10^7 samples in 34s. However, the calculation is also much more imprecise. Because it reuses previous data, the new data is very similar to it, and so it takes a huge number of samples to converge to a more accurate approximation of pi.

--- montecarlo.py
+++ montecarlo.py
@@ -21,22 +21,27 @@ def in_circle(x, origin = [0,0]):
     elif distance(origin, x) <= 1: return True
     else: return False
 
-x = np.logspace(1, 4, num=1000, dtype=int)
+x = list(np.logspace(2, 7, num=100, dtype=int))
 y = []
 
 start_time = time.perf_counter()
+inside = []
 for R in x:
-    inside=[]
     for i in range(R):
-        point = [rand(), rand()]
-        inside.append(in_circle(point))
+        while len(inside) < i:
+            point = [rand(), rand()]
+            inside.append(in_circle(point))
+    #print(len(inside))
     y.append(sum(inside) / R * 4)
 end_time = time.perf_counter()        
     
-print(y)
+print(y[-1])
 
 dt = end_time-start_time
 print()
 print(dt)
 
-plt.plot(x, y)
+fig = plt.figure()
+ax = fig.add_subplot(111)
+#ax.set_xscale("log")
+ax.plot(x, y)
