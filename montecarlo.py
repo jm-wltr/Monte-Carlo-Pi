840cd7c8f9bedede33a7a899066373906287b7f4
jm-wltr <90411864+jm-wltr@users.noreply.github.com>
2021-09-24 12:45:52 +0200

    Changed to log and added time
    Added measure of time to calculate pi values and changed the scale of the number of samples to calculate pi to logarithmic, so it does a lot of calculations with few samples and less with larger samples. This reduces the calculation time by a lot while maintaining the graph more or less the same, since there are so many calculations.

--- montecarlo.py
+++ montecarlo.py
@@ -6,30 +6,37 @@ Created on Fri Sep 24 11:17:10 2021
 @author: jaimewalter
 """
 import random
-import math
 import matplotlib.pyplot as plt
+import numpy as np
+import time
 
 def rand():
     return random.uniform(-1, 1)
 
 def distance(x, y):
-    return math.sqrt((y[0]-x[0])**2 + (y[1]-x[1])**2)
+    return np.sqrt((y[0]-x[0])**2 + (y[1]-x[1])**2)
 
 def in_circle(x, origin = [0,0]):
     if len(x) != 2: return "x is not two-dimensional"
     elif distance(origin, x) <= 1: return True
     else: return False
 
-x = list(range(1, 1000, 1))
+x = np.logspace(1, 4, num=1000, dtype=int)
 y = []
 
+start_time = time.perf_counter()
 for R in x:
     inside=[]
     for i in range(R):
         point = [rand(), rand()]
         inside.append(in_circle(point))
     y.append(sum(inside) / R * 4)
-        
+end_time = time.perf_counter()        
+    
 print(y)
+
+dt = end_time-start_time
+print()
+print(dt)
 
 plt.plot(x, y)
