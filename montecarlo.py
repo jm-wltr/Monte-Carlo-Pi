08d905104e1f110aadf86e24179b292877cf11ff
jm-wltr <90411864+jm-wltr@users.noreply.github.com>
2021-09-25 13:46:45 +0200

    Many fast simulations
    This one uses the fast algorithm multiple times, so in the end it is faster and the graph looks like it should have from the beginning.

--- montecarlo.py
+++ montecarlo.py
@@ -1,8 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
 Created on Fri Sep 24 11:17:10 2021
-
 @author: jaimewalter
 """
 import random
@@ -21,27 +27,29 @@ def in_circle(x, origin = [0,0]):
     elif distance(origin, x) <= 1: return True
     else: return False
 
-x = list(np.logspace(2, 7, num=100, dtype=int))
-y = []
-
-start_time = time.perf_counter()
-inside = []
-for R in x:
-    for i in range(R):
-        while len(inside) < i:
-            point = [rand(), rand()]
-            inside.append(in_circle(point))
-    #print(len(inside))
-    y.append(sum(inside) / R * 4)
-end_time = time.perf_counter()        
-    
-print(y[-1])
-
-dt = end_time-start_time
-print()
-print(dt)
+x = list(np.logspace(1, 3, num=50, dtype=int))
 
 fig = plt.figure()
 ax = fig.add_subplot(111)
-#ax.set_xscale("log")
-ax.plot(x, y)
+#ax.set_xscale("log") 
+   
+for times in range(1,100):
+    y = []
+    
+    start_time = time.perf_counter()
+    inside = []
+    for R in x:
+        for i in range(R):
+            while len(inside) < i:
+                point = [rand(), rand()]
+                inside.append(in_circle(point))
+        y.append(sum(inside) / R * 4)
+    end_time = time.perf_counter()        
+        
+    print(y[-1])
+    
+    dt = end_time-start_time
+    print()
+    print(dt)
+
+    ax.plot(x, y)
