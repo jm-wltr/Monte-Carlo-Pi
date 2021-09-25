212ec81c1ad59440a67f7596eb50700fe5ed9f90
jm-wltr <90411864+jm-wltr@users.noreply.github.com>
2021-09-25 20:03:16 +0200

    Far more accurate value for pi by calculating mean
    It the mean of all the approximations of pi to give a far more accurate value. It also gives more information, like the average of all the approximations that are within one and two standard deviations (although it looks like calculating the mean directly is usually better). Also, now it shows the time that the entire code has taken to run, not how long each calculation takes, as it is always the same and makes the console messy.

--- montecarlo.py
+++ montecarlo.py
@@ -16,6 +16,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 import time
 
+start_time = time.perf_counter()
+
 def rand():
     return random.uniform(-1, 1)
 
@@ -27,29 +29,61 @@ def in_circle(x, origin = [0,0]):
     elif distance(origin, x) <= 1: return True
     else: return False
 
-x = list(np.logspace(1, 3, num=50, dtype=int))
+x = list(np.logspace(1, 4, num=50, dtype=int))
 
 fig = plt.figure()
 ax = fig.add_subplot(111)
+pi = []
 #ax.set_xscale("log") 
    
-for times in range(1,100):
+for times in range(1,1000):
     y = []
-    
-    start_time = time.perf_counter()
+
     inside = []
     for R in x:
         for i in range(R):
             while len(inside) < i:
                 point = [rand(), rand()]
                 inside.append(in_circle(point))
-        y.append(sum(inside) / R * 4)
-    end_time = time.perf_counter()        
+        y.append(sum(inside) / R * 4)       
         
-    print(y[-1])
-    
-    dt = end_time-start_time
-    print()
-    print(dt)
+    pi.append(y[-1])
 
-    ax.plot(x, y)
+    ax.plot(x, y, label = "_nolegend_") 
+    #ax.plot([x[0],x[-1]], [np.pi,np.pi],
+    #        color="red", marker="o", markersize = 3,linestyle = "dashed", linewidth=2, label = "$\pi$")
+
+
+
+sd0 = np.mean(pi)
+
+sd = np.std(pi)
+
+lenTotal = len(pi)
+pi_sd1 = [a for a in pi if a<=sd0+sd and a>=sd0-sd]
+pi_sd2 = [a for a in pi if a<=sd0+2*sd and a>=sd0-2*sd]
+
+sd1 = np.mean(pi_sd1)
+sd2 = np.mean(pi_sd2)
+
+print("Pi: " + str(np.pi))
+print()
+print("Pi approximate: " + str(pi[-1]))
+print("Distance: "+ str(np.pi-pi[-1]))
+print()
+print("Pi mean: " + str(sd0))
+print("Distance: " + str(np.pi-sd0))
+print()
+print("Pi mean (1sd): " + str(sd1))
+print("Distance: " + str(np.pi-sd1))
+print()
+print("Pi mean (2sd): " + str(sd2))
+print("Distance: " + str(np.pi-sd2))
+print()
+print("SD: " + str(sd))
+print(str(len(pi)) + ", " + str(len(pi_sd1))+ ", " + str(len(pi_sd2)))
+
+end_time = time.perf_counter()    
+
+dt = end_time-start_time
+print(dt)   
